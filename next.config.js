81fb17f8b910e143526daf77bd2c5a01592137a0
MrWangJustToDo <2711470541@qq.com>
2021-07-22 17:27:43 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/nextjs-github-pages/' : '',
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -60,7 +60,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
