acdd97ad6cab7afe4177c5f4a30f89d6eb4ec257
MrWangJustToDo <2711470541@qq.com>
2021-07-22 17:42:03 +0800

    fix css prefix

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   // Use the prefix in production and not development.
-  assetPrefix: isProd ? '/nextjs-github-pages/' : '',
+  assetPrefix: isProd ? '/MrWangJustToDo.io/' : '',
 }
