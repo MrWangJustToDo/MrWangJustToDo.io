86d3ed84b41c676f8183068e71b55ad0182b86ad
hengwangsay <hengwangsay@users.noreply.github.com>
2022-05-14 03:11:48 +0000

    deploy: 715425b9ad9d36e8cc3741b81227df04552041d1

--- _next/static/khxEZHiav_3ZZHJxmdg8c/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/993-66c945e6a2798815.js","static/chunks/pages/index-58f74553adc7483a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
