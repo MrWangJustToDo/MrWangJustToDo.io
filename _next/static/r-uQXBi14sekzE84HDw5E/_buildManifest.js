eceeacc8fb443743aff98d1f47af78c5a51cb09b
MrWangJustToDo <MrWangJustToDo@users.noreply.github.com>
2022-05-14 16:40:32 +0000

    deploy: 27e12532b97f70dfa3f017f1726f0c0170aea84d

--- _next/static/r-uQXBi14sekzE84HDw5E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/728-baabf6baab0286a4.js","static/chunks/pages/index-1ebb508ec0cf8ccd.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
