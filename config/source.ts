11d9999369529fd5510df43e357674452391b708
MrWangJustToDo <2711470541@qq.com>
2022-09-10 00:00:31 +0800

    new myreact

--- config/source.ts
+++ config/source.ts
@@ -6,8 +6,8 @@ export const BLOG_REPOSITORY = "MrWangJustToDo.io";
 export const BLOG_REPOSITORY_OWNER = "MrWangJustToDo";
 
 // can be replace here to use React
-export const PLAYGROUND_MY_REACT = resourceUri("myreact.umd.development.js");
-export const PLAYGROUND_MY_REACT_DOM = resourceUri("myreact.umd.development.js");
+export const PLAYGROUND_MY_REACT = resourceUri("myreact.js");
+export const PLAYGROUND_MY_REACT_DOM = resourceUri("myreact-dom.js");
 export const PLAYGROUND_GRID_LAYOUT = resourceUri("grid.js");
 export const PLAYGROUND_BABEL = "https://unpkg.com/@babel/standalone/babel.min.js";
 export const PLAYGROUND_REACT_WINDOW = "https://unpkg.com/react-window@1.8.7/dist/index-dev.umd.js";
