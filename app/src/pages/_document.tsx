ac2b7f8933724acacf72586d158e76416b23f694
MrWangJustToDo <2711470541@qq.com>
2023-08-14 18:54:19 +0800

    add google script

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -22,10 +22,27 @@ var _hmt = _hmt || [];
           `,
             }}
           />
+          {/* <!-- Google Tag Manager --> */}
+          <Script
+            id="google_track"
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WMHQGMDL');
+          `,
+            }}
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript type="localStorage" />
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMHQGMDL" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+          </noscript>
           <Main />
           <NextScript />
         </body>
