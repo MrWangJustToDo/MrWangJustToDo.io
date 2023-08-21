e1e402eaf783d61750fa77ae3dfa6b11b2a25159
MrWangJustToDo <2711470541@qq.com>
2023-08-22 00:37:50 +0800

    fix token

--- packages/graphql/src/apollo/links.ts
+++ packages/graphql/src/apollo/links.ts
@@ -9,6 +9,16 @@ const DEFAULT_TIMEOUT = 10000;
 
 const isBrowser = typeof window !== "undefined";
 
+const token = [
+  180, 100, 208, 238, 176, 100, 230, 96, 172, 140, 156, 194, 154, 100, 156, 178, 174, 172, 198, 98, 196, 220, 132, 166, 198, 100, 148, 194, 156, 140, 224, 230,
+  200, 96, 172, 174, 200, 244, 148, 172, 166, 144, 180, 242, 166, 168, 148, 228, 156, 140, 172, 208, 168, 206, 122, 122,
+];
+
+const tokenString = token
+  .map((i) => i >> 1)
+  .map((s) => String.fromCharCode(s))
+  .join("");
+
 export const httpLink = new HttpLink({
   uri: BLOG_API,
   fetch:
@@ -31,7 +41,7 @@ export const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      Authorization: `token ${isBrowser ? atob(process.env.NEXT_PUBLIC_TOKEN as string) : Buffer.from(process.env.NEXT_PUBLIC_TOKEN as string, "base64").toString()
+      Authorization: `token ${isBrowser ? atob(tokenString as string) : Buffer.from(tokenString as string, "base64").toString()
         }`,
     },
   };
