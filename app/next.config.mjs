5a84f0bf39abec6ac7135d04afb58eedc252de82
MrWangJustToDo <2711470541@qq.com>
2024-09-03 19:49:32 +0800

    update

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,7 +1,8 @@
-import RefreshWebpackPlugin from "@my-react/react-refresh-tools/RefreshWebpackPlugin";
-import { createRequire } from "node:module";
+// import RefreshWebpackPlugin from "@my-react/react-refresh-tools/RefreshWebpackPlugin";
+import withNext from "@my-react/react-refresh-tools/withNext";
+// import { createRequire } from "node:module";
 
-const require = createRequire(import.meta.url);
+// const require = createRequire(import.meta.url);
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -10,79 +11,79 @@ const isProd = process.env.NODE_ENV === "production";
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfigWithMyReact = {
-  reactStrictMode: true,
-  // Use the prefix in production and not development.
-  assetPrefix: isProd ? "/MrWangJustToDo.io/" : undefined,
-  basePath: isProd ? "/MrWangJustToDo.io" : undefined,
-  // i18n router, not work for export
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
-  // custom webpack config
-  webpack: (config, { buildId: _build, dev, isServer, defaultLoaders, nextRuntime: _nextRuntime, webpack }) => {
-    const originalEntry = config.entry;
+// const nextConfigWithMyReact = {
+//   reactStrictMode: true,
+//   // Use the prefix in production and not development.
+//   assetPrefix: isProd ? "/MrWangJustToDo.io/" : undefined,
+//   basePath: isProd ? "/MrWangJustToDo.io" : undefined,
+//   // i18n router, not work for export
+//   // i18n: {
+//   //   locales: ["en"],
+//   //   defaultLocale: "en",
+//   // },
+//   // custom webpack config
+//   webpack: (config, { buildId: _build, dev, isServer, defaultLoaders, nextRuntime: _nextRuntime, webpack }) => {
+//     const originalEntry = config.entry;
 
-    if (dev) {
-      config.entry = async () => {
-        const entries = await originalEntry();
+//     if (dev) {
+//       config.entry = async () => {
+//         const entries = await originalEntry();
 
-        if (entries["main.js"] && !entries["main.js"].includes(require.resolve("@my-react/react-refresh-tools/runtime"))) {
-          entries["main.js"].unshift(require.resolve("@my-react/react-refresh-tools/runtime"));
-        }
+//         if (entries["main.js"] && !entries["main.js"].includes(require.resolve("@my-react/react-refresh-tools/runtime"))) {
+//           entries["main.js"].unshift(require.resolve("@my-react/react-refresh-tools/runtime"));
+//         }
 
-        return entries;
-      };
+//         return entries;
+//       };
 
-      config.plugins.push(new RefreshWebpackPlugin());
-    }
+//       config.plugins.push(new RefreshWebpackPlugin());
+//     }
 
-    // Disable package exports field resolution in webpack. It can lead
-    // to dual package hazards where packages are imported twice: One
-    // commonjs version and one ESM version. This breaks hooks which have
-    // to rely on a singleton by design (nothing we can do about that).
-    // See #25 and https://nodejs.org/dist/latest-v14.x/docs/api/esm.html#esm_dual_package_hazard
-    // for more information.
-    const webpackVersion = webpack.version;
-    if (isServer && +webpackVersion.split(".")[0] >= 5) {
-      config.resolve.exportsFields = [];
-    }
+//     // Disable package exports field resolution in webpack. It can lead
+//     // to dual package hazards where packages are imported twice: One
+//     // commonjs version and one ESM version. This breaks hooks which have
+//     // to rely on a singleton by design (nothing we can do about that).
+//     // See #25 and https://nodejs.org/dist/latest-v14.x/docs/api/esm.html#esm_dual_package_hazard
+//     // for more information.
+//     const webpackVersion = webpack.version;
+//     if (isServer && +webpackVersion.split(".")[0] >= 5) {
+//       config.resolve.exportsFields = [];
+//     }
 
-    if (!defaultLoaders) {
-      throw new Error("This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade");
-    }
+//     if (!defaultLoaders) {
+//       throw new Error("This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade");
+//     }
 
-    // Move Preact into the framework chunk instead of duplicating in routes:
-    const splitChunks = config.optimization && config.optimization.splitChunks;
-    if (splitChunks && splitChunks.cacheGroups) {
-      const cacheGroups = splitChunks.cacheGroups;
-      const test = /[\\/]node_modules[\\/](@my-react|@my-react\/react-dom)[\\/]/;
-      if (cacheGroups.framework) {
-        cacheGroups["@my-react"] = Object.assign({}, cacheGroups.framework, {
-          test,
-        });
-        // if you want to merge the 2 small commons+framework chunks:
-        // cacheGroups.commons.name = 'framework';
-      }
-    }
+//     // Move Preact into the framework chunk instead of duplicating in routes:
+//     const splitChunks = config.optimization && config.optimization.splitChunks;
+//     if (splitChunks && splitChunks.cacheGroups) {
+//       const cacheGroups = splitChunks.cacheGroups;
+//       const test = /[\\/]node_modules[\\/](@my-react|@my-react\/react-dom)[\\/]/;
+//       if (cacheGroups.framework) {
+//         cacheGroups["@my-react"] = Object.assign({}, cacheGroups.framework, {
+//           test,
+//         });
+//         // if you want to merge the 2 small commons+framework chunks:
+//         // cacheGroups.commons.name = 'framework';
+//       }
+//     }
 
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        __SERVER__: isServer,
-        __CLIENT__: !isServer,
-        __DEV__: process.env.NODE_ENV !== "production",
-      }),
-    );
+//     config.plugins.push(
+//       new webpack.DefinePlugin({
+//         __SERVER__: isServer,
+//         __CLIENT__: !isServer,
+//         __DEV__: process.env.NODE_ENV !== "production",
+//       }),
+//     );
 
-    const aliases = config.resolve.alias || (config.resolve.alias = {});
-    aliases.react = "@my-react/react";
-    aliases["react-dom$"] = "@my-react/react-dom";
+//     const aliases = config.resolve.alias || (config.resolve.alias = {});
+//     aliases.react = "@my-react/react";
+//     aliases["react-dom$"] = "@my-react/react-dom";
 
-    // Important: return the modified config
-    return config;
-  },
-};
+//     // Important: return the modified config
+//     return config;
+//   },
+// };
 
 /**
  * @type {import('next').NextConfig}
@@ -92,6 +93,7 @@ const nextConfig = {
   // Use the prefix in production and not development.
   assetPrefix: isProd ? "/MrWangJustToDo.io/" : undefined,
   basePath: isProd ? "/MrWangJustToDo.io" : undefined,
+  output: 'export',
   // custom webpack config
   webpack: (config, { isServer, webpack }) => {
     config.plugins.push(
@@ -112,6 +114,6 @@ const nextConfig = {
 // waiting fix
 // SEE https://github.com/vercel/next.js/issues/19303
 
-const config = process.env.FRAMEWORK === "myreact" ? nextConfigWithMyReact : nextConfig;
+const config = process.env.FRAMEWORK === "myreact" ? withNext(nextConfig) : nextConfig;
 
 export default config;
