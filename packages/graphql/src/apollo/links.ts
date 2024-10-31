d3a859b761eba43d22dc1582389e81d71e5b530d
MrWangJustToDo <2711470541@qq.com>
2024-10-31 13:58:48 +0800

    update

--- packages/graphql/src/apollo/links.ts
+++ packages/graphql/src/apollo/links.ts
@@ -1,7 +1,7 @@
 import { HttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { onError } from "@apollo/client/link/error";
-import { createRequest, generateFetchWithTimeout } from "project-tool/request";
+import { createRequest } from "project-tool/request";
 
 const BLOG_API = "https://api.github.com/graphql";
 
@@ -13,6 +13,30 @@ const token = [
   180, 100, 208, 238, 176, 100, 230, 96, 172, 140, 156, 194, 154, 100, 156, 178, 174, 172, 198, 98, 196, 220, 132, 166, 198, 100, 148, 194, 156, 140, 224, 230,
   200, 96, 172, 174, 200, 244, 148, 172, 166, 144, 180, 242, 166, 168, 148, 228, 156, 140, 172, 208, 168, 206, 122, 122,
 ];
+
+const generateFetchWithTimeout = (timeout: number) => {
+  return (input: RequestInfo, init: RequestInit) => {
+    return new Promise<Response>((resolve, reject) => {
+      const controller = new AbortController();
+      const { signal } = controller;
+
+      const timeoutId = setTimeout(() => {
+        controller.abort();
+        reject(new Error("Request timed out"));
+      }, timeout);
+
+      fetch(input, { ...init, signal })
+        .then((response) => {
+          clearTimeout(timeoutId);
+          resolve(response);
+        })
+        .catch((error) => {
+          clearTimeout(timeoutId);
+          reject(error);
+        });
+    });
+  };
+};
 
 const tokenString = token
   .map((i) => i >> 1)
